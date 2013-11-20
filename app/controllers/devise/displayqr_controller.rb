4744ba0475c7253c8468519b5df57e1187d583ec
Michael Borohovski <borski@tinfoilsecurity.com>
2013-11-20 06:09:57 -0800

    Fixing it so it doesn't 500

--- app/controllers/devise/displayqr_controller.rb
+++ app/controllers/devise/displayqr_controller.rb
@@ -1,17 +1,19 @@
 class Devise::DisplayqrController < DeviseController
   prepend_before_filter :authenticate_scope!, :only => [:show,:update]
-  
+
   include Devise::Controllers::Helpers
-  
+
   def show
     if not resource.nil? and not resource.gauth_secret.nil?
       render :show
+    elsif resource.nil?
+      redirect_to root_path
     else
       sign_in scope, resource, :bypass => true
       redirect_to stored_location_for(scope) || :root
     end
   end
-  
+
   def update
     tmp = params[resource_name]
     if resource.set_gauth_enabled(params[resource_name])
@@ -22,12 +24,12 @@ class Devise::DisplayqrController < DeviseController
       render :show
     end
   end
-    
+
   private
   def scope
     resource_name.to_sym
   end
-  
+
   def authenticate_scope!
     send(:"authenticate_#{resource_name}!")
     self.resource = send("current_#{resource_name}")
